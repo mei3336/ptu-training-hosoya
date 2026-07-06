1e686349c6aaa62e1fce61ee9dbec3b220a64ee3
mei3336 <mei-hosoya@comthink.co.jp>
2026-07-06 16:21:27 +0900

    入力フォームのテンプレ作成完了

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,11 +1,13 @@
 import Button from "../components/Button";
-
+import Input from "../components/Input";
 
 function LoginPage() {
   return (
     <div>
       <h1>ログイン画面</h1>
-      <Button variant="danger">ログイン</Button>
+      <Input label="メールアドレス" name="email" placeholder="メールアドレスを入力してください" />
+      <Input label="パスワード" name="password" type="password" placeholder="パスワードを入力してください" />
+      <Button variant="primary">ログイン</Button>
     </div>
   );
 }
