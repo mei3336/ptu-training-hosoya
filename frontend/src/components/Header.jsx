292269bc01767a5b66ccbc99acb42f684028a718
mei3336 <mei-hosoya@comthink.co.jp>
2026-07-07 13:38:46 +0900

    ヘッダー完成

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,26 @@
+import React from "react";
+import Button from "@/components/Button";
+import { useNavigate } from "react-router-dom";
+
 function Header() {
+  const navigate = useNavigate();
+
   return (
     
     <header className="header">
+      <div className="header-content">
+        <h1 className="header-title">地域コミュニティ</h1>
+        <nav className="header-nav">
+          <a href="/members" className="header-link">メンバー一覧</a>
+          <a href="/mypage" className="header-link">マイページ</a>
+          <a href="/users" className="header-link">名簿管理（管理者専用）</a>
+        </nav>
+        <Button
+          onClick={() => navigate("/")}
+        >
+          ログアウト
+        </Button>
+      </div>
     </header>
   );
 }
